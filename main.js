974bebfdcc24465024003091d29527d56759a450
meadch <meadch@gmail.com>
2016-04-28 16:51:07 -0700

    capture user data and create url

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('hello')
+/*
+##JavaScript Tasks
+Listen for button click/form submit X
+Capture username data X
+Create URL X
+Make an Ajax request
+Give Ajax request a callback function
+
+###Inside the callback function
+Parsing the response from GitHub and creating some DOM elements (h2, ul, li) with the data.
+Add created elements to repo-info
+*/
+
+$(document).ready(function(){
+  $("button").click(function(event){
+    event.preventDefault();
+
+    var username = $("#username").val()
+    var url = `https://api.github.com/users/${username}/repos`
+    console.log(url)
+
+
+
+  })
+})
